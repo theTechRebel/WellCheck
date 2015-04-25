a7679382c226698a153b771cca85787792651cba
Steven Dumbura <stevedumbura@gmail.com>
2015-04-25 17:40:50 +0200

    added all tests to the selection screen

--- application/views/clinician/packages.php
+++ application/views/clinician/packages.php
@@ -86,7 +86,7 @@
                     <tr><td><input type='checkbox' name='comprehensive[]' value='bloodgroup_5'/>Blood Group</td><td>$5.00</td></tr>
                     </tbody>
                     </table>
-                    <input class="btn btn-success" type='submit' name='submit' class='bill' value='Bill Quick Check'/>
+                    <input class="btn btn-success" type='submit' name='submit' class='bill' value='Bill Comprehensive Check'/>
                     </form>
                     </div>
                     </div>
@@ -114,22 +114,122 @@
                     </div>
                 </div>
                 <div class="tab-pane" id="premarital">
-3
+                <form name='package' method='post' action='<?php echo $url?>dashboard/stage/' class='form-packages'>
+                    <table class='table small'>
+                    <td>Description</td><td>Amount</td>
+                    <tbody>
+                    <tr>This package includes all basic tests & all infections tests (MAX 2 screens)+:</tr>
+                    <tr><td><input type='checkbox' name='premarital[]' value='pregnancy_0'/>Pregnancy Test</td><td>$00.00</td></tr>
+                    <tr><td><input type='checkbox' name='premarital[]' value='trigs_20'/>TRIGs</td><td>$20.00</td></tr>
+                    <tr><td><input type='checkbox' name='premarital[]' value='ovulationkit_5'/>Ovulation Test Kit</td><td>$5.00</td></tr>
+                    <tr><td><input type='checkbox' name='premarital[]' value='testosterone'/>Testosterone Test</td><td>TBA</td></tr>
+                    <tr><td><input type='checkbox' name='premarital[]' value='semen'/>Semen Analysis</td><td>TBA</td></tr>
+                    <tr><td><input type='checkbox' name='premarital[]' value='estrogen'/>Estrogen</td><td>TBA</td></tr>
+                    <tr><td><input type='checkbox' name='premarital[]' value='familyplanning_0'/>Family Planning</td><td>$00.00</td></tr>
+                    <tr><td><input type='checkbox' name='premarital[]' value='counselling_15'/>Counselling</td><td>$15.00</td></tr>
+                    <tr><td><input type='checkbox' name='premarital[]' value='bloodgroup_5'/>Blood Group</td><td>$5.00</td></tr>
+                        </tbody>
+                    </table>
+                    <input class="btn btn-success" type='submit' name='submit' class='bill' value='Bill Premarital Check'/>
+                </form>
                 </div>
                 <div class="tab-pane" id="antinetal">
-4
+                <form name='package' method='post' action='<?php echo $url?>dashboard/stage/' class='form-packages'>
+                    <table class='table small'>
+                    <td>Description</td><td>Amount</td>
+                    <tbody>
+                    <tr>This package includes all basic tests +:</tr>
+                    <tr><td><input type='checkbox' name='antinetal[]' value='torchscreen_03'/>TORCH screen</td><td>$30.00</td></tr>
+                    <tr><td><input type='checkbox' name='antinetal[]' value='trigs_20'/>TRIGs</td><td>$20.00</td></tr>
+                    <tr><td><input type='checkbox' name='antinetal[]' value='bloodgroup_5'/>Blood Group</td><td>$5.00</td></tr>
+                    <tr><td><input type='checkbox' name='antinetal[]' value='pregnancy_5'/>Pregnancy Test</td><td>$05.00</td></tr>
+                    <tr><td><input type='checkbox' name='antinetal[]' value='urine_5'/>Urine Test</td><td>$5.00</td></tr>
+                        </tbody>
+                    </table>
+                    <input class="btn btn-success" type='submit' name='submit' class='bill' value='Bill Antinetal Check'/>
+
+                </form>
                 </div>
                 <div class="tab-pane" id="cancer">
-5
+
+                <form name='package' method='post' action='<?php echo $url?>dashboard/stage/' class='form-packages'>
+                    <table class='table small'>
+                    <td>Description</td><td>Amount</td>
+                    <tbody>
+                    <tr>This package includes all basic tests +:</tr>
+                    <tr><td><input type='checkbox' name='cancer[]' value='dre_15'/>PSA +Digital Rectal Examination (DRE)</td><td>$15.00</td></tr>
+                    <tr><td><input type='checkbox' name='cancer[]' value='alphafetoprotein'/>Alpha Feto Protein</td><td>TBA</td></tr>
+                    <tr><td><input type='checkbox' name='cancer[]' value='cea_30'/>CEA</td><td>$30.00</td></tr>
+                    <tr><td><input type='checkbox' name='cancer[]' value='ca125_30'/>Ca-125</td><td>$30.00</td></tr>
+                    <tr><td><input type='checkbox' name='cancer[]' value='viac_10'/>VIAC</td><td>$10.00</td></tr>
+                    <tr><td><input type='checkbox' name='cancer[]' value='hpv'/>HPV</td><td>TBA</td></tr>
+                    <tr><td><input type='checkbox' name='cancer[]' value='breastexam_5'/>Breast Exam</td><td>$5.00</td></tr>
+                    <tr><td><input type='checkbox' name='cancer[]' value='mammography'/>Mammography</td><td>TBA</td></tr>
+                    <tr><td><input type='checkbox' name='cancer[]' value='psa_5'/>PSA</td><td>$10.00</td></tr>
+                        </tbody>
+                    </table>
+                    <input class="btn btn-success" type='submit' name='submit' class='bill' value='Bill Cancer Check'/>
+
+                </form>
                 </div>
                 <div class="tab-pane" id="corporate">
-6
+                <form name='package' method='post' action='<?php echo $url?>dashboard/stage/' class='form-packages'>
+                    <table class='table small'>
+                    <td>Description</td><td>Amount</td>
+                    <tbody>
+                    <tr><td><input type='checkbox' name='corporate[]' value='foodhandlers_50'/>Food Handlers</td><td>$50.00</td></tr>
+                    <tr><td><input type='checkbox' name='corporate[]' value='pneumoconiosis_35'/>Pneumoconiosis</td><td>$35.00</td></tr>
+                    <tr><td><input type='checkbox' name='corporate[]' value='petrolhandlers'/>Petrol Handlers test</td><td>TBA</td></tr>
+                    <tr><td><input type='checkbox' name='corporate[]' value='agrochem_35'/>Agro-Chem Tests</td><td>$35.00</td></tr>
+                    <tr><td><input type='checkbox' name='corporate[]' value='visualscreen_15'/>Visual Screen</td><td>$15.00</td></tr>
+                    <tr><td><input type='checkbox' name='corporate[]' value='audiometry_15'/>Audiometry Tests</td><td>$15.00</td></tr>
+                    <tr><td><input type='checkbox' name='corporate[]' value='spirometry_15'/>Spirometry Tests</td><td>$15.00</td></tr>
+                    <tr><td><input type='checkbox' name='corporate[]' value='stress'/>Stress Test</td><td>TBA</td></tr>
+                    <tr><td><input type='checkbox' name='corporate[]' value='lead_30'/>Lead Test</td><td>$30.00</td></tr>
+                        </tbody>
+                    </table>
+                    <input class="btn btn-success" type='submit' name='submit' class='bill' value='Bill corporate Check'/>
+
+                </form>
                 </div>
                 <div class="tab-pane" id="vaccinations">
-7
+
+                <form name='package' method='post' action='<?php echo $url?>dashboard/stage/' class='form-packages'>
+                    <table class='table small'>
+                    <td>Description</td><td>Amount</td>
+                    <tbody>
+                    <tr><td><input type='checkbox' name='vaccinations[]' value='yellowfever_60'/>Yellow Fever</td><td>$60.00</td></tr>
+                    <tr><td><input type='checkbox' name='vaccinations[]' value='hepatitisa_40'/>Hepatitis A</td><td>$40.00</td></tr>
+                    <tr><td><input type='checkbox' name='vaccinations[]' value='hepatitisb_30'/>Hepatitis B</td><td>$30.00</td></tr>
+                    <tr><td><input type='checkbox' name='vaccinations[]' value='typhoid_40'/>Typhoid</td><td>$40.00</td></tr>
+                    <tr><td><input type='checkbox' name='vaccinations[]' value='mantoux_25'/>Mantoux</td><td>$25.00</td></tr>
+                    <tr><td><input type='checkbox' name='vaccinations[]' value='tetanus_15'/>Tetanus</td><td>$15.00</td></tr>
+                    <tr><td><input type='checkbox' name='vaccinations[]' value='meningitis_30'/>Meningitis</td><td>$30.00</td></tr>
+                    <tr><td><input type='checkbox' name='vaccinations[]' value='roatavirus_60'/>Roatavirus</td><td>$60.00</td></tr>
+                    <tr><td><input type='checkbox' name='vaccinations[]' value='flushot_15'/>Flu shot</td><td>$15.00</td></tr>
+                    <tr><td><input type='checkbox' name='vaccinations[]' value='cervarix_150'/>Cervarix( 3 shots)</td><td>$150.00</td></tr>
+                    <tr><td><input type='checkbox' name='vaccinations[]' value='rabis_35'/>Rabis</td><td>$35.00</td></tr>
+                        </tbody>
+                    </table>
+                    <input class="btn btn-success" type='submit' name='submit' class='bill' value='Bill corporate Check'/>
+
+                </form>
                 </div>
                 <div class="tab-pane" id="drugs">
-8
+                <form name='package' method='post' action='<?php echo $url?>dashboard/stage/' class='form-packages'>
+                    <table class='table small'>
+                    <td>Description</td><td>Amount</td>
+                    <tbody>
+                    <tr>This package includes all basic tests +:</tr>
+                    <tr><td><input type='checkbox' name='drugs[]' value='cannabis_20'/>Cannabis</td><td>$20.00</td></tr>
+                    <tr><td><input type='checkbox' name='drugs[]' value='heroin_20'/>Heroin</td><td>$20.00</td></tr>
+                    <tr><td><input type='checkbox' name='drugs[]' value='amphetamines_20'/>Amphetamines</td><td>$20.00</td></tr>
+                    <tr><td><input type='checkbox' name='drugs[]' value='coccaine_20'/>coccaine</td><td>$20.00</td></tr>
+                        </tbody>
+                    </table>
+                    <input class="btn btn-success" type='submit' name='submit' class='bill' value='Bill Drugs Check'/>
+
+                </form>
                 </div>
             </div>
         </div>
