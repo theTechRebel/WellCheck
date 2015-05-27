6968b3826b5068175ca76ecec055b35c860b789f
Steven Dumbura <stevedumbura@gmail.com>
2015-05-27 10:34:10 +0200

    added image to login page

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,4 @@
-<div class="footer">
+<div class="footer" style="background-color:#F8F8F8">
 <h5><tt><a href="http://www.wellnesscenter.co.zw">Rapha Healthcare Systems: Wellness Application</a></tt></h5>
     <h6><tt>Engineered with &hearts; by <a href="http://www.afrikaizen.co.zw/">AfriKaizen Inc.</a></tt></h6>
 </div>
--- application/views/login.php
+++ application/views/login.php
@@ -2,8 +2,19 @@
 require_once('header.php');		
 ?>
 
+<style>
+body { 
+    background-image:url('http://localhost/wellness/uiux/images/joina.PNG');
+    background-color:#F8F8F8;
+    background-size: auto auto;
+    background-repeat:no-repeat;
+    background-position: center;
+}
+</style>
+
+<body>
 <div class="container">    
-        <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
+        <div id="loginbox" style="margin-top:180px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
             <div class="panel panel-info" >
                     <div class="panel-heading">
                         <div class="panel-title">Welcome to the Wellness Application. PLease Login.</div>
